b5f0883e633f644bfb307835cc7c399a21095aeb
sanketvagal <sanketdvagal@gmail.com>
2023-07-10 16:57:48 -0300

    Added basic Card, used public path for images instead of import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Hero from './components/Hero'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +14,7 @@ function App() {
       <div className='container'>
         <Navbar />
         <Hero />
+        <Card />
       </div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import airbnbLogo from '../assets/airbnb-logo.png';
+// import airbnbLogo from '../assets/airbnb-logo.png';
 
 export default function Navbar() {
     return (
         <nav>
-            <img src={airbnbLogo} className="nav--logo" />
-            {/* <h3 className="nav--logo_text"> ReactFacts </h3>
-            <h4 className="nav--title"> React Course - Project 1</h4> */}
+            <img src={"/assets/airbnb-logo.png"} className="nav--logo" />
         </nav>
     )
 }
