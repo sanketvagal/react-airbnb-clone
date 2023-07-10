55e584131ba045c6d356bec25f25ccc528458e8f
sanketvagal <sanketdvagal@gmail.com>
2023-07-10 18:47:23 -0300

    Passed spread object {...card} as prop to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,28 +2,25 @@ import React from "react";
 
 export default function Card(props) {
     let badgeText
-    if (props.card.openSpots === 0) {
+    if (props.openSpots === 0) {
         badgeText = "SOLD OUT"
-        console.log(badgeText)
-    } else if (props.card.location === "Online") {
+    } else if (props.location === "Online") {
         badgeText = "ONLINE"
-        console.log(badgeText)
-
     }
 
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={"/assets/" + props.card.coverImg} className="card--image" />
-            {/* <img src={`../assets/${props.card.coverImg}`} className="card--image" /> */}
+            <img src={"/assets/" + props.coverImg} className="card--image" />
+            {/* <img src={`../assets/${props.coverImg}`} className="card--image" /> */}
             <div className="card--stats">
                 <img src="/assets/star.png" className="card--star" />
-                <span>{props.card.stats.rating}</span>
-                <span className="gray"> ({props.card.stats.reviewCount}) • </span>
-                <span className="gray"> {props.card.location}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray"> ({props.stats.reviewCount}) • </span>
+                <span className="gray"> {props.location}</span>
             </div>
-            <p>{props.card.title}</p>
-            <p><span className="bold">From ${props.card.price}</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
